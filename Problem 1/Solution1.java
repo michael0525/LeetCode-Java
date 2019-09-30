0982dad349bc774b05db03ef9533020e9c13d58a
Yizhuo Du <michael03160525@gmail.com>
2019-09-29 20:54:01 -0500

    Initial Commit Problem 1

---
+++ Problem 1/Solution1.java
@@ -0,0 +1,29 @@
+/**
+This solution is written by Yizhuo Du
+Sept 29, 2019
+*/
+
+/**
+ * Two-pass HashMap
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+
+import java.util.HashMap;
+
+
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++)
+            map.put(nums[i], i);
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if ( map.containsKey(complement) && map.get(complement) != i)
+                return new int[] {i, map.get(complement)};
+        }
+
+        return new int[0];
+    }
---
+++ Problem 1/Solution2.java
@@ -0,0 +1,10 @@
+/**
+This solution is written by Yizhuo Du
+Sept 29, 2019
+*/
+
+/**
+ * One-pass HashMap
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
